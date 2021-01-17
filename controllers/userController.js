8b6d28556d9be092927e3ce956a2ec9777649506
Visinigiri Aditya <adityakumarverified@gmail.com>
2021-01-17 20:28:05 +0530

    Added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,8 @@
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 module.exports.registerValidations = [
   body("name").not().isEmpty().trim().withMessage("Name is Required"),
   body("email").not().isEmpty().trim().withMessage("Emial is required"),
@@ -6,10 +10,35 @@ module.exports.registerValidations = [
     .isLength({ min: 6 })
     .withMessage("Password Must be 6 Character Long"),
 ];
-module.exports.register = (req, res) => {
+module.exports.register = async (req, res) => {
   const { name, email, password } = req.body;
   const errors = validationResult(req);
   if (!errors.isEmpty) {
-    return res.status(400).json({ errors: errors.array()});
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const checkUser = await User.findOne({ email });
+    if (checkUser) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: "Email is Already Taken" }] });
+    }
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    try {
+      const user = await User.create({
+        name,
+        email,
+        password: hash,
+      });
+      const token = jwt.sign({ user }, process.env.SECRET, {
+        expiresIn: "7d",
+      });
+    } catch (error) {
+      return res.status(500).json({ errors: error });
+    }
+  } catch (error) {
+    return res.status(500).json({ errors: error });
   }
 };
