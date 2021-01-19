f166361f042b6c7e073204994466b9984c76da17
Visinigiri Aditya <adityakumarverified@gmail.com>
2021-01-19 20:55:42 +0530

    Testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./scss/main.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Register from "./components/auth/Register";
